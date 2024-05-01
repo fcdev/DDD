e68493b4f3652fee383e0497c799cbe556ea6ef2
Flávio Couto <flavio.couto@live.com>
2024-05-01 12:15:02 -0300

    Criar DDD.Domain

---
+++ DDD.Domain/Entities/Cliente.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.Entities
+{
+    internal class Cliente
+    {
+        public Cliente(int clienteId, string nome, string sobrenome, string email, DateTime dataCadastro, bool ativo)
+        {
+            ClienteId = clienteId;
+            Nome = nome;
+            Sobrenome = sobrenome;
+            Email = email;
+            DataCadastro = dataCadastro;
+            Ativo = ativo;
+        }
+
+        public int ClienteId { get; private set; }
+        public string Nome { get; private set; }
+        public string Sobrenome { get; private set; }
+        public string Email { get; private set; }
+        public DateTime DataCadastro { get; private set; }
+        public bool Ativo { get; private set; }
+    }
+}
---
+++ DDD.Domain/Interfaces/IClienteRepository.cs
@@ -0,0 +1,20 @@
+﻿using DDD.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.Interfaces
+{
+    internal interface IClienteRepository
+    {
+        void Criar(Cliente cliente);
+        Cliente LerPorId(int id);
+        IEnumerable<Cliente> LerTodos();
+        void Atualizar(Cliente cliente);
+        void Deleter(Cliente cliente);
+
+        Cliente LerPorNome(int id);
+    }
+}
